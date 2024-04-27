4cab0deb3fd08291578b3a9c08a68795850f1841
Md. Sabbir Ahmed <29686831+Sabbir256@users.noreply.github.com>
2024-04-28 02:24:04 +0600

    added data persisting logic - append only file

---
+++ aof.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+type Aof struct {
+	file *os.File
+	rd   *bufio.Reader
+	mu   sync.Mutex
+}
+
+func NewAof(path string) (*Aof, error) {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0666)
+
+	if err != nil {
+		return nil, err
+	}
+
+	aof := &Aof{
+		file: f,
+		rd:   bufio.NewReader(f),
+	}
+
+	go func() {
+		for {
+			aof.mu.Lock()
+			aof.file.Sync()
+			aof.mu.Unlock()
+			time.Sleep(time.Second)
+		}
+	}()
+
+	return aof, nil
+}
+
+func (aof *Aof) Close() error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	return aof.file.Close()
+}
+
+func (aof *Aof) Write(v Value) error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	_, err := aof.file.Write(v.Converter())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (aof *Aof) Read(fn func(Value)) {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+	
+	for {
+		resp := NewRespReader(aof.rd)
+
+		value, err := resp.Read()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fn(value)
+	}
+}
--- main.go
+++ main.go
@@ -16,6 +16,27 @@ func main() {
 		return
 	}
 
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func (value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+
+		if !ok {
+			fmt.Println("Unknown command: " + command)
+			return
+		}
+
+		handler(args)
+	})
+
 	conn, err := ln.Accept()
 	if err != nil {
 		fmt.Println(err)
@@ -53,6 +74,10 @@ func main() {
 			fmt.Println("Unknown command: " + command)
 			writer.Write(Value{typ: "string", str: ""})
 			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
 		}
 
 		result := handler(args)
